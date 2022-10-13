291c807a0bc1e6958146baf056e568a65071f087
Arcticzz <82758121+Arcticzz@users.noreply.github.com>
2022-10-13 19:23:41 +0400

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,40 @@
+const Engine=Matter.Engine;
+const Render=Matter.Render;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+const Constraint=Matter.Constraint;
+const Body=Matter.Body;
+const Composite=Matter.Composite;
+const Composites=Matter.Composites;
+
+let engine;
+let world;
+var ball, blower, blowerMouth, button;
+
+function setup() {
+  createCanvas(800,400);
+  //createSprite(400, 200, 50, 50);
+  engine=Engine.create();
+  world=engine.world;
+
+  ball = new Ball(width / 2 + 80, height / 2 - 80, 80, 80); 
+  blower = new Blower(width / 2 - 50, height / 2 + 50, 150, 20); 
+  blowerMouth = new BlowerMouth(width / 2 + 70, height / 2 + 20, 100, 90); 
+  button = createButton("Click to Blow");
+   button.position(width / 2, height - 100); 
+   button.class("blowButton"); 
+   button.mousePressed(blow);
+
+}
+
+function draw() {
+  background(255,255,255);  
+  Engine.update(engine);
+  blower.show();
+  ball.show();
+  blowerMouth.show();
+}
+
+function blow() {
+Matter.Body.applyForce(ball.body,{x:0,y:0},{x:0, y:0.05});
+}
